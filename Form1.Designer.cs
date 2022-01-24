a7c15d7c932c926e7a55af150389211cd978222a
Damiano Mauro <damiano.mauro@hotmail.com>
2022-01-24 21:12:18 +0100

    primo commit da migliorare bug su automatismo della risposta

---
+++ Form1.Designer.cs
@@ -0,0 +1,327 @@
+﻿namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.panel9 = new System.Windows.Forms.Panel();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.panel6.SuspendLayout();
+            this.panel7.SuspendLayout();
+            this.panel8.SuspendLayout();
+            this.panel9.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 62);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(150, 150);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(168, 62);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(150, 150);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Location = new System.Drawing.Point(324, 62);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(150, 150);
+            this.panel3.TabIndex = 1;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.label6);
+            this.panel4.Location = new System.Drawing.Point(324, 218);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(150, 150);
+            this.panel4.TabIndex = 4;
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.label5);
+            this.panel5.Location = new System.Drawing.Point(168, 218);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(150, 150);
+            this.panel5.TabIndex = 3;
+            // 
+            // panel6
+            // 
+            this.panel6.Controls.Add(this.label4);
+            this.panel6.Location = new System.Drawing.Point(12, 218);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(150, 150);
+            this.panel6.TabIndex = 2;
+            // 
+            // panel7
+            // 
+            this.panel7.Controls.Add(this.label9);
+            this.panel7.Location = new System.Drawing.Point(324, 374);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(150, 150);
+            this.panel7.TabIndex = 7;
+            // 
+            // panel8
+            // 
+            this.panel8.Controls.Add(this.label8);
+            this.panel8.Location = new System.Drawing.Point(168, 374);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(150, 150);
+            this.panel8.TabIndex = 6;
+            // 
+            // panel9
+            // 
+            this.panel9.Controls.Add(this.label7);
+            this.panel9.Location = new System.Drawing.Point(12, 374);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(150, 150);
+            this.panel9.TabIndex = 5;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 19);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(61, 17);
+            this.radioButton1.TabIndex = 8;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Cerchio";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(150, 46);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Squadra";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(73, 19);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(53, 17);
+            this.radioButton2.TabIndex = 9;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Croce";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 105F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 150);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "X";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 105F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(150, 150);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "X";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 105F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(0, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(150, 150);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "X";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 105F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(0, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(150, 150);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "X";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 105F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(0, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(150, 150);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "X";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 105F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(0, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(150, 150);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "X";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 105F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(0, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(150, 150);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "X";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 105F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(0, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(150, 150);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "X";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // label9
+            // 
+            this.label9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 105F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(0, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(150, 150);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "X";
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(486, 536);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel7);
+            this.Controls.Add(this.panel8);
+            this.Controls.Add(this.panel9);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            this.panel6.ResumeLayout(false);
+            this.panel7.ResumeLayout(false);
+            this.panel8.ResumeLayout(false);
+            this.panel9.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Panel panel9;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        String cerchioCroce = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+                cerchioCroce = "X";
+            else
+                cerchioCroce = "O";
+
+            nuovoGioco();
+        }
+
+        void generaRandom()
+        {
+            String segnoOpposto = "";
+
+            if (cerchioCroce == "X")
+                segnoOpposto = "O";
+            else
+                segnoOpposto = "X";
+
+            //genero il segno opposto in una label random, esclusa quella appena cliccata
+            Random r = new Random();
+            int randomNum = r.Next(1, 9);
+
+            if (randomNum == 1)
+            {
+                if (label1.Text == "")
+                    label1.Text = segnoOpposto;
+                else
+                    randomNum++;
+
+            }
+            else if (randomNum == 2)
+            {
+                if (label2.Text == "")
+                    label2.Text = segnoOpposto;
+                else
+                    randomNum++;
+            } else
+            if (randomNum == 3)
+            {
+                if (label3.Text == "")
+                    label3.Text = segnoOpposto;
+                else
+                    randomNum++;
+            }
+            else
+            if (randomNum == 3)
+            {
+                if (label3.Text == "")
+                    label3.Text = segnoOpposto;
+                else
+                    randomNum++;
+            }
+            else
+            if (randomNum == 4)
+            {
+                if (label4.Text == "")
+                    label4.Text = segnoOpposto;
+                else
+                    randomNum++;
+            }
+            else
+            if (randomNum == 5)
+            {
+                if (label5.Text == "")
+                    label5.Text = segnoOpposto;
+                else
+                    randomNum++;
+            }
+            else
+            if (randomNum == 6)
+            {
+                if (label6.Text == "")
+                    label6.Text = segnoOpposto;
+                else
+                    randomNum++;
+            }
+            else
+            if (randomNum == 7)
+            {
+                if (label7.Text == "")
+                    label7.Text = segnoOpposto;
+                else
+                    randomNum++;
+            }
+            else
+            if (randomNum == 8)
+            {
+                if (label8.Text == "")
+                    label8.Text = segnoOpposto;
+                else
+                    randomNum++;
+            }
+            else
+            if (randomNum == 9)
+            {
+                if (label9.Text == "")
+                    label9.Text = segnoOpposto;
+                else
+                    randomNum++;
+            }
+
+
+        }
+
+        void controllaVincita()
+        {
+            Boolean vinto = false;
+
+            //=============Controllo vincita in orizzontale
+            if (label1.Text == cerchioCroce && label2.Text == cerchioCroce && label3.Text == cerchioCroce)
+                vinto = true;
+
+            if (label4.Text == cerchioCroce && label5.Text == cerchioCroce && label6.Text == cerchioCroce)
+                vinto = true;
+
+            if (label7.Text == cerchioCroce && label8.Text == cerchioCroce && label9.Text == cerchioCroce)
+                vinto = true;
+
+            //=============controllo vincita in verticale
+            if (label1.Text == cerchioCroce && label4.Text == cerchioCroce && label7.Text == cerchioCroce)
+                vinto = true;
+
+            if (label2.Text == cerchioCroce && label5.Text == cerchioCroce && label8.Text == cerchioCroce)
+                vinto = true;
+
+            if (label3.Text == cerchioCroce && label6.Text == cerchioCroce && label9.Text == cerchioCroce)
+                vinto = true;
+
+            //=============controllo vincita in obliquo
+            if (label1.Text == cerchioCroce && label5.Text == cerchioCroce && label9.Text == cerchioCroce)
+                vinto = true;
+
+            if (label3.Text == cerchioCroce && label5.Text == cerchioCroce && label7.Text == cerchioCroce)
+                vinto = true;
+
+
+            if (vinto == true)
+            {
+                MessageBox.Show("Hai vinto!");
+                nuovoGioco();
+            }
+        }
+
+        void nuovoGioco()
+        {
+            label1.Text = "";
+            label2.Text = "";
+            label3.Text = "";
+
+            label4.Text = "";
+            label5.Text = "";
+            label6.Text = "";
+
+            label7.Text = "";
+            label8.Text = "";
+            label9.Text = "";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            if(label1.Text == "")
+            label1.Text = cerchioCroce;
+
+            generaRandom();
+            controllaVincita();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            if (label2.Text == "")
+                label2.Text = cerchioCroce;
+
+            generaRandom();
+            controllaVincita();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            if (label3.Text == "")
+                label3.Text = cerchioCroce;
+
+            generaRandom();
+            controllaVincita();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            if (label4.Text == "")
+                label4.Text = cerchioCroce;
+
+            generaRandom();
+            controllaVincita();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            if (label5.Text == "")
+                label5.Text = cerchioCroce;
+
+            generaRandom();
+            controllaVincita();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+            if (label6.Text == "")
+                label6.Text = cerchioCroce;
+
+            generaRandom();
+            controllaVincita();
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+            if (label7.Text == "")
+                label7.Text = cerchioCroce;
+
+            generaRandom();
+            controllaVincita();
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+            if (label8.Text == "")
+                label8.Text = cerchioCroce;
+
+            generaRandom();
+            controllaVincita();
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+            if (label9.Text == "")
+                label9.Text = cerchioCroce;
+
+            generaRandom();
+            controllaVincita();
+        }
+    }
+}
